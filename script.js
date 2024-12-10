127f9e5a32f5741f61a9b43222eea5e131d1f434
ManabBiswas <156077951+ManabBiswas@users.noreply.github.com>
2024-12-10 23:58:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function toggleTheme() {
     const body = document.body;
     const buttons = document.querySelectorAll('.theme-btn button, .menu-btn button');
     const menuBtnSpans = document.querySelectorAll('.menu-btn span');
+    const logo = document.getElementById('logo');
     const navLinks = document.querySelectorAll('nav ul li a');
 
     const themes = [
@@ -19,7 +20,7 @@ function toggleTheme() {
 
     currentTheme = nextTheme.name;
     body.className = nextTheme.class;
-
+// console.log(currentTheme);
     buttons.forEach(button => {
         button.style.borderColor = nextTheme.buttonColor;
         button.style.color = nextTheme.buttonColor;
@@ -32,6 +33,13 @@ function toggleTheme() {
     navLinks.forEach(link => {
         link.style.color = nextTheme.navLinkColor;
     });
+
+    // Update logo based on current theme
+    if (currentTheme !=="dark") {
+        logo.src = "img/light-theme-logo1.png";
+    } else {
+        logo.src = "img/logo.png";
+    }
 }
 
 function toggleMenu() {
@@ -46,3 +54,6 @@ document.getElementById('current-year').textContent = new Date().getFullYear();
 
 // Set default theme on page load
 document.body.className = 'theme-default';
+
+// Set logo based on default theme
+document.getElementById('logo').src = "img/light-theme-logo.png";
